129977c80dd34c773638f95b486fae8580b3fc6d
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-10-12 18:30:17 +0300

    update

--- public/js/app.js
+++ public/js/app.js
@@ -87,6 +87,68 @@ module.exports = __webpack_require__(3);
 window.$ = window.jQuery = __webpack_require__(2);
 window.Laravel = { csrfToken: $('meta[name=csrf-token]').attr("content") };
 
+/* HEADER ON SCROLL >>
+=================================================================================================== */
+
+$(function () {
+
+    function setHeaderColor() {
+        $('body').addClass('scroll');
+    }
+
+    function removeHeaderColor() {
+        $('body').removeClass('scroll');
+    }
+
+    function updateScrollHeader() {
+        if ($(window).scrollTop() > 5) {
+            setHeaderColor();
+        } else {
+            removeHeaderColor();
+        }
+    }
+
+    updateScrollHeader();
+    $(window).scroll(updateScrollHeader);
+});
+
+/* HEADER MENU >>
+=================================================================================================== */
+
+$(function () {
+
+    function openMenu() {
+
+        $('header').append('<div class="modal_bg"></div>');
+        $('nav, .modal_bg').css({ 'height': $(window).outerHeight() + 100 });
+        $('body').addClass('menu-open');
+    }
+
+    function closeMenu() {
+        $('body').removeClass('menu-open');
+        $('.modal_bg').detach();
+    }
+
+    $(document).on('click', '#button-menu,.modal_bg', function () {
+
+        if ($('body').hasClass('menu-open')) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    });
+
+    $(window).resize(function () {
+
+        if ($(window).width() > 768) {
+            closeMenu();
+            $('nav, .modal_bg').removeAttr('style');
+        } else {
+            $('nav, .modal_bg').css({ 'height': $(window).outerHeight() + 100 });
+        }
+    });
+});
+
 /***/ }),
 /* 2 */
 /***/ (function(module, exports, __webpack_require__) {
--- resources/js/app.js
+++ resources/js/app.js
@@ -9,3 +9,72 @@
 =================================================================================================== */
 window.$ = window.jQuery = require('jquery');
 window.Laravel = { csrfToken: $('meta[name=csrf-token]').attr("content") };
+
+
+
+/* HEADER ON SCROLL >>
+=================================================================================================== */
+
+$(function () {
+
+    function setHeaderColor() {
+        $('body').addClass('scroll');
+    }
+
+    function removeHeaderColor() {
+        $('body').removeClass('scroll');
+    }
+
+    function updateScrollHeader() {
+        if ($(window).scrollTop() > 5) {
+            setHeaderColor()
+        } else {
+            removeHeaderColor()
+        }
+    }
+
+    updateScrollHeader();
+    $(window).scroll(updateScrollHeader);
+
+});
+
+
+/* HEADER MENU >>
+=================================================================================================== */
+
+$(function () {
+
+    function openMenu() {
+
+        $('header').append('<div class="modal_bg"></div>');
+        $('nav, .modal_bg').css({'height': $(window).outerHeight() + 100});
+        $('body').addClass('menu-open');
+    }
+
+    function closeMenu() {
+        $('body').removeClass('menu-open');
+        $('.modal_bg').detach();
+    }
+
+    $(document).on('click', '#button-menu,.modal_bg', function () {
+
+        if ($('body').hasClass('menu-open')) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+
+    });
+
+    $(window).resize(function () {
+
+        if ($(window).width() > 768) {
+            closeMenu();
+            $('nav, .modal_bg').removeAttr('style');
+        } else {
+            $('nav, .modal_bg').css({'height': $(window).outerHeight() + 100});
+        }
+
+    });
+
+});
